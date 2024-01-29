7fe17b9f46f6c8401ec66c6fbdc50a51c16a82ba
Caroline Akesson <ca223pw@student.lnu.se>
2024-01-28 18:06:45 -0600

    Make request to API for hardcoded text

--- ProductDescriptionApi/Controllers/ProductDescriptionController.cs
+++ ProductDescriptionApi/Controllers/ProductDescriptionController.cs
@@ -3,17 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 namespace ProductDescriptionApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+// [Route("[controller]")]
+[Route("product-description")]
 public class ProductDescriptionController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
     {
-       var response = new { Message = "Welcome to this api " };
+       var response = new { Message = "Hello! You are talking to the API that will generate product descriptions using AI" };
 
             // Return the anonymous object as a JSON result
             return Ok(response);
-    }
-
-    
+    }    
 }
---
+++ ProductDescriptionFrontend/Components/Models/ApiResponse.cs
@@ -0,0 +1,6 @@
+namespace ProductDescriptionFrontend;
+
+public class ApiResponse
+{
+  public string Message { get; set; }
+}
